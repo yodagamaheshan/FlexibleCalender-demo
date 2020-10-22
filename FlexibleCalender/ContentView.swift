6ead9aa7b608f2fe8bcdb5aa245333480811617b
Heshan Yodagama <yodagamaheshan@icloud.com>
2020-10-22 09:37:59 +0530

    Initial Commit

---
+++ FlexibleCalender/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FlexibleCalender
+//
+//  Created by Heshan Yodagama on 10/22/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FlexibleCalender/FlexibleCalenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlexibleCalenderApp.swift
+//  FlexibleCalender
+//
+//  Created by Heshan Yodagama on 10/22/20.
+//
+
+import SwiftUI
+
+@main
+struct FlexibleCalenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
