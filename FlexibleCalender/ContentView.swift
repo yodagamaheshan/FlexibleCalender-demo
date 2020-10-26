2a41c199a14d353f90421aa9685b786320f1a2f2
Heshan Yodagama <yodagamaheshan@icloud.com>
2020-10-26 12:59:01 +0530

    days, months, years view

--- FlexibleCalender/ContentView.swift
+++ FlexibleCalender/ContentView.swift
@@ -12,11 +12,21 @@ struct ContentView: View {
     @State var mode = CalenderMode.week(estimateHeight: 400)
     var body: some View {
         VStack {
-            Text(selectedMonthDate.description)
+            Text(DateFormatter.monthAndYear.string(from: selectedMonthDate))
+            HStack {
+                ForEach(Calendar.current.veryShortWeekdaySymbols, id: \.self) { item in
+                    Spacer()
+                    Text(item)
+                        .bold()
+                    Spacer()
+                    
+                }
+            }
             FlexibleCalenderView(interval: .init(start: Date.getDate(from: "2020 01 11")!, end: Date.getDate(from: "2020 12 11")!), selectedMonth: $selectedMonthDate, mode: mode) { date in
                 
                 DateCell1(date: date)
             }
+            .padding()
             HStack {
                 Button("Month") {
                     mode = .month(estimateHeight: 400)
