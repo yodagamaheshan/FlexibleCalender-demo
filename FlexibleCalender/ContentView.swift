1b1bbdbbae8cb3442f5cfb5ee6107492f17f9bdc
Heshan Yodagama <yodagamaheshan@icloud.com>
2020-10-26 08:59:01 +0530

    DateCell

--- FlexibleCalender/ContentView.swift
+++ FlexibleCalender/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
             Text(selectedMonthDate.description)
             FlexibleCalenderView(interval: .init(start: Date.getDate(from: "2019 08 11")!, end: Date.getDate(from: "2021 08 11")!), selectedMonth: $selectedMonthDate, mode: mode) { date in
                 
-                DateCell(date: date)
+                DateCell1(date: date)
             }
             Button("Change mode") {
                 mode = (mode == .month ? CalenderMode.week:.month)
@@ -27,30 +27,5 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-    }
-}
-
-struct DateCell: View {
-    @State private var isShown = false
-    let date: Date
-    
-    var body: some View {
-        VStack {
-            if isShown {
-                Text(date.day)
-                    .padding(8)
-                    .background(Color.blue)
-                    .cornerRadius(20)
-                    .transition(AnyTransition.scale.animation(Animation.spring().delay(Double(date.day)!/30)))
-                
-            } else {
-                Text("")
-                    .onAppear(perform: {
-                        withAnimation {
-                            isShown = true
-                        }
-                    })
-            }
-        }
     }
 }
