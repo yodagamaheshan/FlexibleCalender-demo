194a77d3bf0f51dc89547bc0f3c1872814d9f6c9
Heshan Yodagama <yodagamaheshan@icloud.com>
2020-10-30 11:37:39 +0530

    add weekday symbols

---
+++ FlexibleCalender/WeekDaySymbols.swift
@@ -0,0 +1,20 @@
+//
+//  WeekDaySymbols.swift
+//  FlexibleCalender
+//
+//  Created by Heshan Yodagama on 10/30/20.
+//
+
+import SwiftUI
+
+struct WeekDaySymbols: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WeekDaySymbols_Previews: PreviewProvider {
+    static var previews: some View {
+        WeekDaySymbols()
+    }
+}
