17b98a6a6d536c3571836b58357e5c564e6a068a
Heshan Yodagama <yodagamaheshan@icloud.com>
2020-10-30 11:52:09 +0530

    extract date cell

---
+++ FlexibleCalender/Extension/NoramalDayCell.swift
@@ -0,0 +1,20 @@
+//
+//  NoramalDayCell.swift
+//  FlexibleCalender
+//
+//  Created by Heshan Yodagama on 10/30/20.
+//
+
+import SwiftUI
+
+struct NoramalDayCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NoramalDayCell_Previews: PreviewProvider {
+    static var previews: some View {
+        NoramalDayCell()
+    }
+}
